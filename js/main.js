205abad5d7335765c04be0780e7673dde82b2e8f
ISazhyna <seoirinas@gmail.com>
2020-01-21 21:55:27 +0200

    view changes

--- js/main.js
+++ js/main.js
@@ -1,39 +1,36 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
-
- $(".navbar a").click(function(){
+    $(".navbar a").click(function(){
     $("body,html").animate({
  	    scrollTop:$("#" + $(this).data('value')).offset().top-100
  	},1500)
-  
- })
-
+    })
 })
 
 //horizontal tabs
 function openStory(evt, storyName) {
-    var i, tabcontent, tablinks;
+    let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
     document.getElementById(storyName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
-//document.querySelector(".replace-image").addEventListener( "click", function(e) {
+//#1 swap word to img and vice verse
 $('.replace-image').click(function (e) {
     let thisId=this.parentElement.getAttribute("id");
     let fullQuery=document.getElementById(thisId).querySelector(".replace-words");
     console.log(fullQuery);
     if (e.target && e.target.getAttribute("changed")=="false"){
-        var str = fullQuery.innerHTML;
-        var regexOb = new RegExp('\\b'+e.target.alt+'(|s)'+'\\b',"gi"); 
-        var res = str.replace(regexOb, '<img class="replaced" alt="'+e.target.alt+'" src="images/replace/'+e.target.alt+'.png">');
+        let str = fullQuery.innerHTML;
+        let regexOb = new RegExp('\\b'+e.target.alt+'(|s)'+'\\b',"gi"); 
+        let res = str.replace(regexOb, '<img class="replaced" alt="'+e.target.alt+'" src="images/replace/'+e.target.alt+'.png">');
         fullQuery.innerHTML = res;
         e.target.setAttribute("changed", "true");
         e.target.setAttribute("src", "images/replace/"+e.target.alt+".png");
@@ -41,21 +38,20 @@ $('.replace-image').click(function (e) {
     else {
         e.target.setAttribute("src", "images/replace/"+e.target.alt+"-bw.png");
         e.target.setAttribute("changed", "false");
-        var str =fullQuery.innerHTML;
-        var path='<img class="replaced" alt="'+e.target.alt+'" src="images/replace/'+e.target.alt+'.png">';
-        var regexOb = new RegExp(path,"gi"); 
-        var res = str.replace(regexOb, e.target.alt);
+        let str =fullQuery.innerHTML;
+        let path='<img class="replaced" alt="'+e.target.alt+'" src="images/replace/'+e.target.alt+'.png">';
+        let regexOb = new RegExp(path,"gi"); 
+        let res = str.replace(regexOb, e.target.alt);
        fullQuery.innerHTML = res;
     }
 });
 
 //#3 match the shadow game
-var dragged; 
-var itemId;
-var success=document.querySelector("#success");
-
+let dragged; 
+let itemId;
+let success=document.querySelector("#success");
 function onDragOver(event) {
-  event.preventDefault();
+    event.preventDefault();
 }
 
 function onDragLeave(event) {
@@ -71,11 +67,10 @@ function onDragEnter(event) {
     }
 }
 
-var count = 0;
+let count = 0;
 function onDrop(event) {
     const target = event.target;
-    console.log(target);
-    console.log(dragged);
+    console.log("onDrop occurred");
     if (target && dragged) {
         target.style.opacity = '';
         event.preventDefault();
@@ -87,12 +82,12 @@ function onDrop(event) {
         count++;
     }
     success.textContent= dragged.getAttribute("fruit");
-    if (count==7){
-    setTimeout(function(){ success.textContent="Horray!" }, 1500);
-    }
+        if (count==7){
+            setTimeout(function(){ success.textContent="Horray!" }, 1500);
+        }
 }
 
-function test(a){
+function addTargetId(a){
     const dropZone = a;
     dropZone.addEventListener('drop', onDrop);
     dropZone.addEventListener('dragenter', onDragEnter);
@@ -108,10 +103,8 @@ function onDragStart(event) {
         event.dataTransfer.dropEffect = 'move';
         event.target.style.opacity = .3;
     }
-    test(document.querySelector("."+target.id));
+    addTargetId(document.querySelector("."+target.id));
 }
-
-
 
 function onDragEnd(event) {
     if (event.target && event.target.nodeName === 'DIV'){
@@ -148,7 +141,7 @@ theThing.addEventListener("click", function(event) {
 	    theThing.style.top = "300px";
         theThing.style.width = "170px";
 	    theThing.style.height = "90px";
-        var auto_refresh = setInterval(
+        let auto_refresh = setInterval(
             function () {
                 if (document.querySelector("[traffic]").getAttribute("traffic")=="1") {
                     $("#thing").removeClass("transition").css({"left":"0px", "top":"0px", "width":"90px", "height":"50px"});
@@ -163,7 +156,7 @@ theThing.addEventListener("click", function(event) {
     }
 });
 
-//overlay
+//overlay on clicking over yellow or red
 function on(color) {
     console.log(color);
     document.getElementById("overlay").style.display = "block";
