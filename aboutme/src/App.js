bf19a8b23306fcd247ea62b60c0b9376bab98327
[isazhyna] <[seoirinas@gmail.com]>
2020-01-31 23:12:02 +0200

    initial routing

---
+++ aboutme/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import logo from './logo.svg';
+import Projects from './Projects';
+import About from './About';
+import Home from './Home';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
